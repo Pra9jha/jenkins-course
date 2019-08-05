b6ccb7c3c76bbe99d523520b5a06d53d13de50fc
Prashant Jha <prashant@engagedly.com>
2019-08-05 17:37:06 +0530

    nodejs.groovy nodejsdocker.groovy modified

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,6 +1,6 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/Pra9jha/jenkins_with_node') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
