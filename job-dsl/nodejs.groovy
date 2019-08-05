cb75591da093af3199f976f2c5a051072e0bac50
Prashant Jha <prashant@engagedly.com>
2019-08-05 18:30:47 +0530

    nodejs.groovy modified

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -13,6 +13,7 @@ job('NodeJS example') {
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
-        shell("npm install")
+        shell("npm install && npm start")
+ 
     }
 }
