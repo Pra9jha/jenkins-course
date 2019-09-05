517c6f93cac479343dd8f221200386727fb7a12d
Pra9jha <41165408+Pra9jha@users.noreply.github.com>
2019-09-05 19:06:01 +0530

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -2,8 +2,14 @@ job('Jenkins_DSL_Example') {
     scm {
         git('https://github.com/Pra9jha/jenkins_with_node.git') {  node -> // is hudson.plugins.git.GitSCM   
             node / gitConfigName('DSL User')
-            branch(master)
         }
+        
+        branches {
+            branchSpec {
+               name(master)
+            }
+        }
+        
     }
     triggers {
         githubPush()
