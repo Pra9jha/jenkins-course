96a569e7a8a5c5649a99f81b5021381638313f96
Pra9jha <41165408+Pra9jha@users.noreply.github.com>
2019-09-05 18:21:46 +0530

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,18 +1,20 @@
-job('Jenkins_DSL_Example') {
+job('NodeJS example') {
     scm {
-        git('https://github.com/Pra9jha/jenkins_with_node.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/Pra9jha/jenkins_with_node') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
     }
     triggers {
-        scm('* * * * *')
+        githubPush()
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
-        
-        shell("sudo apt install npm -y &&  sudo apt-get install nodejs -y &&  sudo  npm install express -y && sudo npm install")
+        shell("sudo npm install")
         shell("sudo  npm start")
  
     }
 }
-
